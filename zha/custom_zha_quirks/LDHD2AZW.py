68599a1f3d269f6cf972d977ee9aed2a7335f502
Josh K <28068117+iamjoshk@users.noreply.github.com>
2024-10-05 14:15:34 -0400

    Update LDHD2AZW.py

--- zha/custom_zha_quirks/LDHD2AZW.py
+++ zha/custom_zha_quirks/LDHD2AZW.py
@@ -1,39 +1,92 @@
-{
-  "node_descriptor": {
-    "logical_type": 2,
-    "complex_descriptor_available": 0,
-    "user_descriptor_available": 0,
-    "reserved": 0,
-    "aps_flags": 0,
-    "frequency_band": 8,
-    "mac_capability_flags": 128,
-    "manufacturer_code": 4381,
-    "maximum_buffer_size": 82,
-    "maximum_incoming_transfer_size": 82,
-    "server_mask": 11264,
-    "maximum_outgoing_transfer_size": 82,LDHD2AZW
-    "descriptor_capability_field": 0
-  },
-  "endpoints": {
-    "1": {
-      "profile_id": "0x0104",
-      "device_type": "0x0402",
-      "input_clusters": [
-        "0x0000",
-        "0x0001",
-        "0x0003",
-        "0x0020",
-        "0x0402",
-        "0x0500",
-        "0x0b05",
-        "0xfd50"
-      ],
-      "output_clusters": [
-        "0x0019"
-      ]
+from typing import Any, List, Optional, Union
+
+from zigpy.profiles import zha
+from zigpy.quirks import CustomCluster, CustomDevice
+import zigpy.types as t
+from zigpy.zcl import foundation
+from zigpy.zcl.clusters.general import (
+    Basic,
+    Groups,
+    Identify,
+    LevelControl,
+    OnOff,
+    PollControl,
+    PowerConfiguration,
+)
+
+from zhaquirks import Bus
+from zhaquirks.const import (
+    ARGS,
+    COMMAND,
+    COMMAND_OFF,
+    COMMAND_ON,
+    COMMAND_STEP,
+    DEVICE_TYPE,
+    DIM_DOWN,
+    DIM_UP,
+    DOUBLE_PRESS,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    LONG_PRESS,
+    MODELS_INFO,
+    OUTPUT_CLUSTERS,
+    PARAMS,
+    PROFILE_ID,
+    SHORT_PRESS,
+    TURN_OFF,
+    TURN_ON,
+    ZHA_SEND_EVENT,
+)
+
+
+
+    signature = {
+        MODELS_INFO: [("Leedarson", "LDHD2AZW")],
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: "0x0104",
+                DEVICE_TYPE: "0x0402",
+                INPUT_CLUSTERS: [
+                  "0x0000",
+                  "0x0001",
+                  "0x0003",
+                  "0x0020",
+                  "0x0402",
+                  "0x0500",
+                  "0x0b05",
+                  "0xfd50"
+                ]
+                OUTPUT_CLUSTERS: [
+                  "0x0019",
+                ],
+            },
+        },
     }
-  },
-  "manufacturer": "Leedarson",
-  "model": "LDHD2AZW",
-  "class": "zigpy.device.Device"
-}
+
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: "0x0402",
+                INPUT_CLUSTERS: [
+                  "0x0000",
+                  "0x0001",
+                  "0x0003",
+                  "0x0020",
+                  "0x0402",
+                  "0x0500",
+                  "0x0b05",
+                  "0xfd50"
+                ]
+                OUTPUT_CLUSTERS: [
+                  "0x0019",
+                  "0x0020",
+                  "0x0039",
+                  "0x0038",
+                  "0x0037",
+                  "0x0036",
+                  "0x0035",
+                ],
+            },
+        },
+    }
