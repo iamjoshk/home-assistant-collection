d06bfb756dec878eabb81d6395997735a65691f0
Josh K <28068117+iamjoshk@users.noreply.github.com>
2024-10-06 12:38:17 -0400

    Update LDHS2AZW_v2.py

--- zha/custom_zha_quirks/LDHS2AZW_v2.py
+++ zha/custom_zha_quirks/LDHS2AZW_v2.py
@@ -4,13 +4,13 @@ from zigpy.quirks.v2 import QuirkBuilder
 (
   
   QuirkBuilder("Leedarson", "LDHD2AZW")
-    .number(
-      PowerConfiguration.AttributeDefs.battery_voltage.name,
-      PowerConfiguration.cluster_id,
-      min_value=0,
-      max_value=100,
-      step=1,
-      unit="v",
+  .number(
+    PowerConfiguration.AttributeDefs.battery_voltage.name,
+    PowerConfiguration.cluster_id,
+    min_value=0,
+    max_value=100,
+    step=1,
+    unit="v",
   )
   .skip_configuration()
   .add_to_registry()
