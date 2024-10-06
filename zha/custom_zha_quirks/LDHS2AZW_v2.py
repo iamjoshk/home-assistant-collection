2649050ca4afa94c2a5783e4f8aa5520bcc5dd75
Josh K <28068117+iamjoshk@users.noreply.github.com>
2024-10-06 14:11:14 -0400

    Update LDHS2AZW_v2.py

--- zha/custom_zha_quirks/LDHS2AZW_v2.py
+++ zha/custom_zha_quirks/LDHS2AZW_v2.py
@@ -3,31 +3,10 @@ from zigpy.quirks.v2 import QuirkBuilder
 from zigpy.zcl.clusters.general import (
     PowerConfiguration
 )
-
-from zigpy.device import Device
-from zigpy.exceptions import MultipleQuirksMatchException
-from zigpy.profiles import zha
-from zigpy.quirks import CustomCluster, CustomDevice, signature_matches
-from zigpy.quirks.registry import DeviceRegistry
 from zigpy.quirks.v2 import (
-    BinarySensorMetadata,
-    CustomDeviceV2,
-    EntityMetadata,
-    EntityPlatform,
-    EntityType,
-    NumberMetadata,
-    SwitchMetadata,
-    WriteAttributeButtonMetadata,
-    ZCLCommandButtonMetadata,
-    ZCLSensorMetadata,
     add_to_registry_v2,
 )
-import zigpy.types as t
-from zigpy.zcl import ClusterType
-from zigpy.zcl.clusters.homeautomation import Diagnostic
-from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef, ZCLCommandDef
 
-from .async_mock import sentinel
 
 
 (
