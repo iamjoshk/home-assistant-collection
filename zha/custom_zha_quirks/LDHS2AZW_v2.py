4ffec61876e588da850b7d4998769b0525c50a9e
Josh K <28068117+iamjoshk@users.noreply.github.com>
2024-10-06 13:09:41 -0400

    Update LDHS2AZW_v2.py

--- zha/custom_zha_quirks/LDHS2AZW_v2.py
+++ zha/custom_zha_quirks/LDHS2AZW_v2.py
@@ -4,17 +4,38 @@ from zigpy.zcl.clusters.general import (
     PowerConfiguration
 )
 
+from zigpy.device import Device
+from zigpy.exceptions import MultipleQuirksMatchException
+from zigpy.profiles import zha
+from zigpy.quirks import CustomCluster, CustomDevice, signature_matches
+from zigpy.quirks.registry import DeviceRegistry
+from zigpy.quirks.v2 import (
+    BinarySensorMetadata,
+    CustomDeviceV2,
+    EntityMetadata,
+    EntityPlatform,
+    EntityType,
+    NumberMetadata,
+    SwitchMetadata,
+    WriteAttributeButtonMetadata,
+    ZCLCommandButtonMetadata,
+    ZCLSensorMetadata,
+    add_to_registry_v2,
+)
+import zigpy.types as t
+from zigpy.zcl import ClusterType
+from zigpy.zcl.clusters.homeautomation import Diagnostic
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeDef, ZCLCommandDef
+
+from .async_mock import sentinel
+
 
 (
   
   QuirkBuilder("Leedarson", "LDHD2AZW")
-  .number(
+  .sensor(
     PowerConfiguration.AttributeDefs.battery_voltage.name,
     PowerConfiguration.cluster_id,
-    min_value=0,
-    max_value=100,
-    step=1,
-    unit="v",
   )
   .skip_configuration()
   .add_to_registry()
