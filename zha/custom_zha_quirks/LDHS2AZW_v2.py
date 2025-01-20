8a700d91fb019d6695c1281c4f3fad58e4a43b97
Josh K <28068117+iamjoshk@users.noreply.github.com>
2025-01-20 13:55:25 -0500

    Update LDHS2AZW_v2.py

--- zha/custom_zha_quirks/LDHS2AZW_v2.py
+++ zha/custom_zha_quirks/LDHS2AZW_v2.py
@@ -1,22 +1,42 @@
 from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.zcl.clusters.general import PowerConfiguration
+from zhaquirks import PowerConfigurationCluster
 
-from zigpy.zcl.clusters.general import (
-    PowerConfiguration
-)
-from zigpy.quirks.v2 import (
-    add_to_registry_v2,
-)
+class CustomPowerConfigurationCluster(PowerConfigurationCluster):
+    """Custom PowerConfigurationCluster to handle battery percentage."""
 
+    def _update_attribute(self, attrid, value):
+        super()._update_attribute(attrid, value)
+        if attrid == self.BATTERY_VOLTAGE_ATTR and value not in (0, 255):
+            super()._update_attribute(
+                self.BATTERY_PERCENTAGE_REMAINING,
+                self._calculate_battery_percentage(value),
+            )
 
+    def _calculate_battery_percentage(self, raw_value):
+        volts = raw_value / 10
+        volts = max(volts, self.MIN_VOLTS)
+        volts = min(volts, self.MAX_VOLTS)
+
+        percent = round(
+            ((volts - self.MIN_VOLTS) / (self.MAX_VOLTS - self.MIN_VOLTS)) * 200
+        )
+
+        self.debug(
+            "Voltage [RAW]:%s [Max]:%s [Min]:%s, Battery Percent: %s",
+            raw_value,
+            self.MAX_VOLTS,
+            self.MIN_VOLTS,
+            percent / 2,
+        )
+
+        return percent
 
 (
-  
-  QuirkBuilder("Leedarson", "LDHD2AZW")
-  .sensor(
-    PowerConfiguration.AttributeDefs.battery_voltage.name,
-    PowerConfiguration.cluster_id,
-  )
-  .skip_configuration()
-  .add_to_registry()
-
+    QuirkBuilder("Leedarson", "LDHD2AZW")
+    .cluster(
+        CustomPowerConfigurationCluster
+    )
+    .skip_configuration()
+    .add_to_registry()
 )
