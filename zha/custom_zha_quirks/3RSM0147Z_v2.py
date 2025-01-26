4fe542f6aebd73180f1f887f7edd4bc70c27773b
Josh K <28068117+iamjoshk@users.noreply.github.com>
2025-01-26 00:41:01 -0500

    simplified quirk

--- zha/custom_zha_quirks/3RSM0147Z_v2.py
+++ zha/custom_zha_quirks/3RSM0147Z_v2.py
@@ -1,24 +1,21 @@
 """Third Reality 3RSM0147Z soil moisture and temperature sensor."""
-from zigpy.quirks.v2 import QuirkBuilder
-from zigpy.quirks.v2.homeassistant import PERCENTAGE, EntityType, EntityPlatform
-from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass, SensorStateClass
-from zigpy.zcl.clusters.measurement import RelativeHumidity
-from zigpy.zcl.clusters.general import Basic
 
+from zigpy.quirks.v2 import QuirkBuilder
+
+from zigpy.zcl.clusters.measurement import SoilMoisture
+
+from zhaquirks import CustomCluster
+
+
+
+class SoilMoistureCluster(SoilMoisture, CustomCluster):
+    """Soil Moisture Cluster for TR"""
+    
+    cluster_id = 1029
 
 (
     QuirkBuilder("Third Reality, Inc", "3RSM0147Z")
-    .replaces(RelativeHumidity)
-    .sensor(
-        RelativeHumidity.AttributeDefs.measured_value.name,
-        RelativeHumidity.cluster_id,
-        multiplier=0.01,
-        device_class=SensorDeviceClass.MOISTURE,
-        state_class=SensorStateClass.MEASUREMENT,
-#        unit=PERCENTAGE, ### uncomment once patched by https://github.com/zigpy/zha/issues/327 ###
-        translation_key="moisture",
-        fallback_name="Soil Moisture"
-    )
+    .replaces(SoilMoistureCluster, 1029, endpoint_id=1)
     .add_to_registry()
 )
 
